b9934a416d74e7a330c09748d0f4767932a4fe0d
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2019-02-17 19:01:28 +0100

    feat(loop.py)

---
+++ forLoop/loop.py
@@ -0,0 +1,12 @@
+cats = ["Eustach", "Lee", "Midnight"]
+for cat in cats:
+  print(cat)
+
+for i in range(1,10):
+  print(i)
+
+dogs = ["King", "Boss"]
+i = 0
+while i < len(dogs):
+  print(dogs[i])
+  i = i + 1
