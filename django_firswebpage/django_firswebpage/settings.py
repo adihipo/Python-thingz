72c085cc12a894ed7c5bfe627e21ebef02ce7c47
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2019-02-19 18:38:24 +0100

    style(first Django page): admin site style

--- django_firswebpage/django_firswebpage/settings.py
+++ django_firswebpage/django_firswebpage/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_firswebpage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC+01'
+TIME_ZONE = 'CET'
 
 USE_I18N = True
 
